1a459c31e239f45df94053aa419322cb24af2f81
ParkSeongjin <mememe5074@gmail.com>
2020-07-27 23:22:54 +0900

    2020/07/27

--- app/src/main/java/com/example/s_park/MarkerItem.java
+++ app/src/main/java/com/example/s_park/MarkerItem.java
@@ -3,14 +3,19 @@ package com.example.s_park;
 public class MarkerItem {
     double lat;
     double lon;
-    boolean state;
+    String state;
     String numOfpark;
+    String userOfpark;
+    String phoneOfpark;
 
-    public MarkerItem(double lat, double lon, boolean state, String numOfpark){
+
+    public MarkerItem(double lat, double lon, String state, String numOfpark, String phoneOfpark, String userOfpark){
         this.lat = lat;
         this.lon = lon;
         this.state = state;
         this.numOfpark = numOfpark;
+        this.userOfpark = userOfpark;
+        this.phoneOfpark = phoneOfpark;
     }
 
     public double getLat() { return lat; }
@@ -18,10 +23,18 @@ public class MarkerItem {
     public double getLon() { return lon; }
     public void setLon(double lon) { this.lon = lon; }
 
-    public boolean getState() { return state; }
-    public void setState(boolean state) { this.state = state; }
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
 
     public String getNumOfpark() {return numOfpark;}
     public void setNumOfpark(String numOfpark){ this.numOfpark = numOfpark;}
+
+    public String getNameOfpark() {return userOfpark;}
+
+    public void setNameOfpark(String userOfpark) {this.userOfpark = userOfpark; }
+
+    public String getPhoneOfpark() {return phoneOfpark;}
+
+    public void setPhoneOfpark(String phoneOfpark) {this.phoneOfpark = phoneOfpark;}
 }
 
